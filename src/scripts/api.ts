9d08565f8d4fb8eccfbfebb5bbc72860c1c46623
Yourz <crazilou@vip.qq.com>
2026-02-13 20:49:47 +0800

    fix: update for design reviews

--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -237,6 +237,7 @@ export type GlobalSubgraphData = {
     search_aliases?: string[]
   }
   data: string | Promise<string>
+  essentials_category?: string
 }
 
 function addHeaderEntry(headers: HeadersInit, key: string, value: string) {
